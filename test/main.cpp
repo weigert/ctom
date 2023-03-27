0ded56f53099d0d8c28f826d03c1213b63b05adb
Nick <nicholasmcdonald40@gmail.com>
2023-03-27 22:50:58 +0200

    cleaned up the ref aliases. now I need to make it so that the references also work for derived types, so that I can define derived types for each stages and compose those

--- test/main.cpp
+++ test/main.cpp
@@ -5,23 +5,31 @@ int main( int argc, char* args[] ) {
 	// Abstract Object Models
 
 	struct A: ctom::obj_impl<
-		ctom::ref_val<"sub-sub-int", int>,
-		ctom::ref_val<"sub-sub-float", float>,
-		ctom::ref_val<"sub-sub-double", double>
+		ctom::val<"sub-sub-int", int>,
+		ctom::val<"sub-sub-float", float>,
+		ctom::val<"sub-sub-double", double>
 	>{};
 
 	struct B: ctom::obj_impl<
-		ctom::ref_obj<"sub-obj", A>
+		ctom::obj<"sub-obj", A>,
+		ctom::val<"sub-char", char>
 	>{};
 
 	struct T: ctom::obj_impl <
-		ctom::ref_val<"valA", int>,
-		ctom::ref_arr<"array", int>,
-		ctom::ref_obj<"object", B>,
-		ctom::ref_val<"valB", int>
+		ctom::val<"valA", int>,
+		ctom::arr<"array", int>,
+		ctom::obj<"object", B>,
+		ctom::val<"valB", int>
 	>{};
 
+	std::cout<<"(static)"<<std::endl;
+	ctom::print<T>();
+
 	// Concrete Model Translation
+
+	struct B_derived: B {
+		char c = B::val<"sub-char">('y');
+	};
 
 	struct T_derived: T {
 		int t = T::val<"valA">(10);
@@ -29,13 +37,16 @@ int main( int argc, char* args[] ) {
 		B b = T::obj<"object">(B{});
 	} Tval;
 
+	std::cout<<std::endl<<"(instance)"<<std::endl;
+	ctom::print(Tval);
+
 	/*
 			Now I need to write an actual proper printer...
 			And arrays need to work too...
 			And I need to get the type copying to also work,
 			i.e. a parser thingy.
 	*/
-	ctom::print<T>();
+
 
 	return 0;
 
